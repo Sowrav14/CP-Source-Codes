c1a9f8e7164597b56b987e1fa9b07f3469bf3601
Sowrav14 <Sowravnath05102002@gmail.com>
2024-01-20 18:53:35 +0600

    1/20/2024

--- Codeforces/test4.cpp
+++ Codeforces/test4.cpp
@@ -1,69 +1,65 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define int long long int
+#define Fast_IO() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-#define Sowrav_Nath ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define f(i,n)              for(int i=0;i<n;i++)
-#define f1(i,n)             for(int i=1;i<=n;i++)
-#define rf(i,n)             for(int i=n-1;i>=0;i--)
-#define rf1(i,n)            for(int i = n;i>=1;i--)
-#define le_debut            signed main
-// #define int                 long long int
-#define endl                '\n'
-#define pb                  push_back
-#define oui                 cout << "YES" << endl
-#define non                 cout << "NO" << endl
-#define homme               cout << "Bob" << endl
-#define femme               cout << "Alice" << endl
-#define un_de_minus         cout << "-1" << endl
-#define duck                cout << 0 << endl;
-#define reponse             cout << ans << endl
-#define ici                 cout << "Je suis ici" << endl
-const int N = 2*1e5+10;
-const int M = 1e9+7;
-const int inf = 1e12;
-
-
-// Inline Function
-inline int bigmod(int x,int y);
-inline int inverse_mod(int n,int M) 			{return bigmod(n,M-2);}
-inline int ad(int x,int y)      				{int ret=(x%M+y%M)%M;   if(ret<0)   {ret+=M,ret=ret%M;} return ret;}
-inline int gun(int x,int y)     				{int ret=((x%M)*(y%M))%M;   if(ret<0)   {ret+=M,ret=ret%M;} return ret;}
-inline int sub(int x,int y)     				{int ret=((x%M)-(y%M)+M)%M;    if(ret<0)    {ret+=M,ret=ret%M;} return ret;}
-inline int bigmod(int x,int y)  				{int ret=1; while(y>0)  {if(y&1)    {ret=(ret*x)%M;}    y>>=1;x=(x*x)%M;}   return ret;}
-inline int egcd(int a,int b,int &x,int &y)		{if(a==0){ x=0; y=1; return b;} int x1,y1; int d=egcd(b%a,a,x1,y1); x=y1-(b/a)*x1; y=x1; return d;}
+bool cmp(pair<int,int>a, pair<int,int>b){
+    if(a.second != b.second){
+        return a.second < b.second;
+    } else {
+        return a.first > b.first;
+    }
+}
 
 
 
-void allons_y(){
+void solve(){
 
-    string s; cin >> s;
-    int n = s.size();
+    int n; cin >> n;
+    vector<pair<int,int>>v;
+    for(int i=1;i<=n;i++){
+        int x,y; cin >> x >> y;
+        v.push_back({x,y});
+    }
+    int ans = 0;
+    int l = 0, r = 1e12;
+    while(l <= r){
+        int m = (l + r) / 2;
 
-    int st = 0;
-    for(int i=n-1; i>=0; i--){
-        if(s[i] == ')'){
-            st++;
-        } else {
-            if(st <= 0){
-                cout << "impossible" << '\n';
-                return;
+        vector<pair<int,int>>less, more;
+        for(int i=0;i<n;i++){
+            if(v[i].first < m){
+                less.push_back(v[i]);
             } else {
-                st--;
+                more.push_back(v[i]);
             }
         }
+        sort(less.begin(), less.end(), cmp);
+        sort(more.begin(), more.end(), cmp);
+
+        if(less.size() == 0){
+            ans = m;
+            l = m + 1;
+            continue;
+        }
+
+        for(int i=0;i<less.size();i++){
+            int wt = less[i].second;
+        }
     }
-    cout << s << '\n';
+    
+
+    
 
 }
 
 
-le_debut(){
-    Sowrav_Nath
-    int test = 1;
-    cin >> test;
-    f1(tc,test){
-        // cout << "Case " << tc << ": ";
-        // memset(dp, 0, sizeof(dp));
-        allons_y();
+signed main(){
+    Fast_IO()
+    int t = 1;
+    cin >> t;
+    for(int i=1;i<=t;i++){
+        // cout << "Case " << t << ": ";
+        solve();
     }
 }
