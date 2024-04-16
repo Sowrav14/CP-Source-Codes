6a94037fbf31fc9d607f657d50119944809b60ad
Sowrav14 <Sowravnath05102002@gmail.com>
2024-04-16 19:03:02 +0600

    123

--- Vjudge/G_G.cpp
+++ Vjudge/G_G.cpp
@@ -12,7 +12,7 @@ inline int sub(int x,int y)     				{int ret=((x%M)-(y%M)+M)%M;    if(ret<0)    
 inline int bigmod(int x,int y)  				{int ret=1; while(y>0)  {if(y&1)    {ret=(ret*x)%M;}    y>>=1;x=(x*x)%M;}   return ret;}
 inline int egcd(int a,int b,int &x,int &y)		{if(a==0){ x=0; y=1; return b;} int x1,y1; int d=egcd(b%a,a,x1,y1); x=y1-(b/a)*x1; y=x1; return d;}
 const int N = 1e5 + 10;
-int bit[30];
+int bit[32];
 int a[N];
 int d[N];
 // updating a single value in bit
@@ -34,10 +34,14 @@ int query(int idx){
 }
 
 int sum(int a, int d, int n){
-    int x = 2 * a + (n - 1) * d;
-    x *= n;
-    x /= 2;
-    return x;
+    int x = gun(2, a);
+    int y = sub(n, 1);
+    y = gun(y, d);
+    int z = ad(x, y);
+    z = gun(z, n);
+    int p = inverse_mod(2, M);
+    z = gun(z, p);
+    return z;
 }
 
 
@@ -46,34 +50,30 @@ void solve(){
 
     string s; cin >> s;
     int n; cin >> n;
-    int ans = 0;
 
     for(int i=0;i<s.size();i++){
         int cur = s[i] - 'a' + 1;
         int x = query(29) - query(cur);
-        // a[i] = x;
-        ans += x;
+        a[i] = x;
         update(cur, 1);
     }
 
-    // for(int i=0;i<s.size();i++){
-    //     int cur = s[i] - 'a' + 1;
-    //     int x = query(29) - query(cur);
-    //     d[i] = x - a[i];
-    //     update(cur, 1);
-    // }
+    for(int i=0;i<s.size();i++){
+        int cur = s[i] - 'a' + 1;
+        int x = query(29) - query(cur);
+        d[i] = x - a[i];
+        update(cur, 1);
+    }
 
     // for(int i=0;i<s.size();i++) cout << a[i] << " "; cout << endl;
     // for(int i=0;i<s.size();i++) cout << d[i] << " "; cout << endl;
 
-    // int ans = 0;
-    // for(int i=0;i<n;i++){
-    //     int x = sum(a[i], d[i], n);
-    //     ans = ad(ans, x);
-    // }
-    // cout << ans << endl;
-
-    
+    int ans = 0;
+    for(int i=0;i<s.size();i++){
+        int x = sum(a[i], d[i], n);
+        ans = ad(ans, x);
+    }
+    cout << ans << endl;
 
 }
 
